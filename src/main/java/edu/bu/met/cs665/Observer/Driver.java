df993ea3d9e758b552e0c2fbbd5974022f8f4225
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-09-30 02:23:26 -0400

    implemented logic for displayNotification and update

--- src/main/java/edu/bu/met/cs665/Observer/Driver.java
+++ src/main/java/edu/bu/met/cs665/Observer/Driver.java
@@ -1,12 +1,16 @@
 package edu.bu.met.cs665.Observer;
 
+import edu.bu.met.cs665.Observable.DeliveryRequest;
+import edu.bu.met.cs665.Observable.Shop;
+
 /**
  * This class represents a single driver.
  */
 public class Driver implements ShopObserver, DisplayNotification {
 
   private String driverName;
-
+  protected Shop shop;
+  protected DeliveryRequest dr;
   /**
    * A default constructor.
    */
@@ -44,14 +48,30 @@ public class Driver implements ShopObserver, DisplayNotification {
 
   @Override
   public void displayNotification() {
-
+    System.out.println(" [Basic Notification] " + " Driver Name: " + getDriverName() + " Shop Name: " + shop.getShopName() + " Customer Name: " + dr.getCustomerName() +
+        " Product Name: " + dr.getProductName() + " Address Line 1: " + dr.getAddressLine1() + " Address Line 2: " + dr.getAddressLine2());
   }
 
 
   @Override
-  public void update() {
-
+  public void update(String shopName, String customerName, String productName, String addressLine1, String addressLine2) {
+    this.dr = new DeliveryRequest(customerName, productName, addressLine1, addressLine2);
+    shop.addDeliveryRequest(dr);
   }
 
+  public Shop getShop() {
+    return shop;
+  }
 
+  public void setShop(Shop shop) {
+    this.shop = shop;
+  }
+
+  public DeliveryRequest getDr() {
+    return dr;
+  }
+
+  public void setDr(DeliveryRequest dr) {
+    this.dr = dr;
+  }
 }
