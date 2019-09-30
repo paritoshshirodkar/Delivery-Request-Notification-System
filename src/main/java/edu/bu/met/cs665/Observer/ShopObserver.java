e942fb6e070e0771f6fca7b0f88f37f6b74e0d12
paritoshshirodkar <paritoshshirodkar@gmail.com>
2019-09-30 02:24:24 -0400

    updated method signature

--- src/main/java/edu/bu/met/cs665/Observer/ShopObserver.java
+++ src/main/java/edu/bu/met/cs665/Observer/ShopObserver.java
@@ -2,6 +2,6 @@ package edu.bu.met.cs665.Observer;
 
 public interface ShopObserver {
 
-  public void update();
+  public void update(String shopName, String customerName, String productName, String addressLine1, String addressLine2);
 
 }
